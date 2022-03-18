d364eefd58866c10bd333def972edceeb647504b
berkinakkaya <berkin_akkaya@hotmail.com>
2022-03-18 20:07:34 +0300

    polish
    polish

--- pages/index.vue
+++ pages/index.vue
@@ -331,7 +331,7 @@ header {
       gap: 20px;
       align-items: flex-end;
 
-      div {
+      .card {
         background-color: white;
         padding: 30px;
         box-shadow: 0 0 25px rgba(0, 0, 0, 0.1);
@@ -340,33 +340,95 @@ header {
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        text-align: center;
+
+        h5 {
+          font-weight: 700;
+          text-align: center;
+          font-size: 1.4em;
+
+          @media (max-width: 1200px) {
+            font-size: 1.1em;
+          }
+        }
 
         p {
-          margin: 0;
+          margin: 0 auto;
+          font-size: 1.2em;
+          padding: 20px 20px 0 20px;
+
+          @media (max-width: 1200px) {
+            font-size: 1em;
+            text-align: center;
+            padding: 5px 5px 0 5px;
+          }
+        }
+
+        .footer {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          width: 100%;
+          max-width: 300px;
+          margin: 20px auto 0 auto;
+
+          @media (max-width: 900px) {
+            display: inline-flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 10px;
+          }
+
+          .customer,
+          .brand {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+
+            b {
+              font-size: 0.9em;
+            }
+
+            img {
+              width: 42px;
+              border-radius: 100%;
+
+              @media (max-width: 900px) {
+                width: 36px;
+              }
+            }
+          }
         }
       }
     }
 
-    .row-1 div:nth-child(odd),
-    .row-2 div:nth-child(even) {
-      min-width: 300px;
+    .row-1 .card:nth-child(odd),
+    .row-2 .card:nth-child(even) {
+      min-width: 33vw;
+
+      @media (max-width: 1200px) {
+        min-width: 500px;
+      }
 
       @media (max-width: 900px) {
-        min-width: 250px;
+        min-width: 280px;
       }
     }
-    .row-1 div:nth-child(even),
-    .row-2 div:nth-child(odd) {
-      min-width: 230px;
+    .row-1 .card:nth-child(even),
+    .row-2 .card:nth-child(odd) {
+      min-width: 25vw;
+
+      @media (max-width: 1200px) {
+        min-width: 350px;
+      }
 
       @media (max-width: 900px) {
-        min-width: 250px;
+        min-width: 280px;
       }
     }
 
     .row-2 {
-      padding-left: 200px;
+      padding-left: 80px;
+      align-items: flex-start;
 
       @media (max-width: 900px) {
         padding-left: 0;
@@ -376,6 +438,9 @@ header {
 }
 
 #compare {
+  position: relative;
+  margin-top: 100px;
+
   .inner {
     background-color: #5379ff;
     color: white;
@@ -384,8 +449,15 @@ header {
     text-align: center;
     padding: 100px 300px 150px 100px;
     min-width: 60vw;
-    margin-top: 100px;
     margin-right: var(--section-padding-inline-negative);
+    position: relative;
+
+    h1,
+    p,
+    #inputs {
+      z-index: 1 !important;
+      position: relative;
+    }
 
     p {
       max-width: 700px;
@@ -404,24 +476,24 @@ header {
       display: flex;
       align-items: center;
       justify-content: center;
-      gap: 50px;
+      gap: 40px;
       margin: 50px 0;
-    }
 
-    #inputs select {
-      padding: 10px 30px;
-      width: 250px;
-      border-radius: 100px;
-      border: none;
-      box-shadow: 0 0 8px rgba($color: #000000, $alpha: 0.1);
-    }
+      input {
+        padding: 12px 30px;
+        width: 250px;
+        border-radius: 100px;
+        border: none;
+        box-shadow: 0 0 8px rgba($color: #000000, $alpha: 0.1);
+      }
 
-    #inputs button {
-      padding: 12px 30px;
-      border-radius: 100px;
-      background-color: #ff5777;
-      color: white;
-      border: none;
+      button {
+        padding: 12px 30px;
+        border-radius: 100px;
+        background-color: #ff5777;
+        color: white;
+        border: none;
+      }
     }
 
     @media (max-width: 1400px) {
@@ -461,10 +533,42 @@ header {
       }
     }
   }
+
+  .circle-big,
+  .circle-small {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    background: #6690fd;
+    border: 60px solid #5f86fe;
+    border-radius: 100%;
+  }
+
+  .circle-big {
+    right: -300px;
+    width: 650px;
+
+    @media (max-width: 1400px) {
+      width: 540px;
+      right: -280px;
+    }
+  }
+
+  .circle-small {
+    width: 650px;
+    right: 400px;
+    transform: scale(0.75);
+    opacity: 0.8;
+
+    @media (max-width: 1400px) {
+      width: 540px;
+    }
+  }
 }
 
 #most_talked_about {
   margin-top: 150px;
+  position: relative;
 
   @media (max-width: 1400px) {
     margin-top: 100px;
@@ -472,16 +576,106 @@ header {
 
   @media (max-width: 900px) {
     margin-top: 70px;
+    padding: 0 10px;
   }
 
   .inner {
     margin: 50px 0;
 
     .card {
-      height: 400px;
-      box-shadow: 0 0 15px rgba($color: #000000, $alpha: 0.1);
+      box-shadow: 0 0 10px rgba($color: #000000, $alpha: 0.15);
       background: white;
       transform: scale(0.9);
+      padding: 50px 30px;
+      border-radius: 10px;
+
+      .heading {
+        font-weight: 700;
+        text-align: center;
+      }
+
+      p {
+        font-size: 1.2em;
+        padding: 25px 50px;
+        margin-top: 25px;
+
+        @media (max-width: 500px) {
+          padding: 25px 0;
+          text-align: justify;
+        }
+      }
+
+      .footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        max-width: 300px;
+        margin: 20px auto 0 auto;
+
+        @media (max-width: 400px) {
+          display: inline-flex;
+          flex-direction: column;
+          align-items: flex-start;
+          gap: 10px;
+        }
+
+        .customer,
+        .brand {
+          display: flex;
+          align-items: center;
+          gap: 10px;
+
+          b {
+            font-size: 0.9em;
+          }
+
+          img {
+            width: 42px;
+            border-radius: 100%;
+
+            @media (max-width: 900px) {
+              width: 36px;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .abstract-circle,
+  .abstract-left {
+    position: absolute;
+    top: 100px;
+    bottom: 0;
+    z-index: -1;
+  }
+
+  .abstract-left {
+    left: 0;
+    width: 50vw;
+    background: #ef476f;
+    border-radius: 0 100% 100% 0;
+  }
+
+  .abstract-circle {
+    left: calc(50vw + 180px);
+    border-radius: 100%;
+    width: 520px;
+    background: #ffd166;
+  }
+
+  @media (max-width: 1400px) {
+    .abstract-left {
+      width: 60vw;
+    }
+    .abstract-circle {
+      display: none;
+    }
+  }
+  @media (max-width: 900px) {
+    .abstract-left {
+      width: 90vw;
     }
   }
 }
@@ -546,149 +740,53 @@ header {
 
       <div class="inner">
         <div class="trending-row row-1">
-          <div>
+          <div v-for="i in 10" :key="i" class="card">
             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
               bibendum risus, sit ac velit purus lectu
             </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
+            <div class="footer">
+              <div class="customer">
+                <img src="../static/customer.png" />
+                <div>
+                  <b>Samet</b>
+                  <br />
+                  <span>12.123 <i class="fas fa-eye"></i></span>
+                </div>
+              </div>
+              <div class="brand">
+                <img src="../static/trendyol.png" />
+                <div>
+                  <b>Trendyol</b>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div class="trending-row row-2">
-          <div>
+          <div v-for="i in 10" :key="i" class="card">
             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
               bibendum risus, sit ac velit purus lectu
             </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
-          </div>
-          <div>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
-              bibendum risus, sit ac velit purus lectu
-            </p>
+            <div class="footer">
+              <div class="customer">
+                <img src="../static/customer.png" />
+                <div>
+                  <b>Samet</b>
+                  <br />
+                  <span>12.123 <i class="fas fa-eye"></i></span>
+                </div>
+              </div>
+              <div class="brand">
+                <img src="../static/trendyol.png" />
+                <div>
+                  <b>Trendyol</b>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -705,38 +803,57 @@ header {
         </p>
 
         <div id="inputs">
-          <select>
-            <option value="">1.Marka</option>
-          </select>
+          <input type="text" placeholder="1. Marka" />
           <button>Karşılaştır</button>
-          <select>
-            <option value="">2.Marka</option>
-          </select>
+          <input type="text" placeholder="2. Marka" />
         </div>
       </div>
+
+      <div class="circle-big"></div>
+      <div class="circle-small"></div>
     </section>
 
     <section id="most_talked_about">
       <h1>Çok Konuşulanlar</h1>
 
       <div class="inner">
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
+        <div class="card" v-for="i in 10" :key="i">
+          <h4 class="heading">
+            Şikayet Başlığı dolor sit amet, consectetur adipiscing elit. Duis
+            dui.
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis
+            bibendum risus, sit ac velit purus lectus lobortis
+          </p>
+          <div class="footer">
+            <div class="customer">
+              <img src="/_nuxt/static/customer.png" />
+              <div>
+                <b>Samet</b> <br />
+                <span>12.123 <i class="fas fa-eye"></i></span>
+              </div>
+            </div>
+            <div class="brand">
+              <img src="/_nuxt/static/trendyol.png" />
+              <div><b>Trendyol</b></div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div class="abstract-left"></div>
+      <div class="abstract-circle"></div>
     </section>
+
+    <div style="height: 300px"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data: () => ({}),
   mounted() {
     console.log($('#most_talked_about .inner').slick)
     $('#most_talked_about .inner').slick({
