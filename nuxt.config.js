f4fe5994f713aa11fc49f562505c32bdf5b34e23
berkinakkaya <berkin_akkaya@hotmail.com>
2022-03-22 23:39:36 +0300

    move font to head

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: '//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css' },
       { rel: 'stylesheet', href: '//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css' },
+      { rel: 'preconnect', href: '//fonts.googleapis.com' },
+      { rel: 'preconnect', href: '//fonts.gstatic.com' },
+      { rel: 'stylesheet', href: '//fonts.googleapis.com/css2?family=Poppins:ital,wght0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' },
     ],
     script: [
       { src: '//code.jquery.com/jquery-3.6.0.min.js' },
