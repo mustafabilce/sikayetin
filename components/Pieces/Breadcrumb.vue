ce0b605b43921f3539b742051b22eeaf94301232
berkinakkaya <berkin_akkaya@hotmail.com>
2022-03-28 20:33:46 +0300

    breadcrumb

---
+++ components/Pieces/Breadcrumb.vue
@@ -0,0 +1,33 @@
+<style lang="scss" scoped>
+#breadcrumb {
+  padding-inline: var(--section-padding-inline);
+  margin-bottom: 50px;
+
+  a {
+    font-weight: 600;
+    color: #00244b80;
+
+    &:hover {
+      color: #00244b;
+    }
+
+    &:not(:last-child)::after {
+      content: '>';
+    }
+  }
+}
+</style>
+
+<template>
+  <div id="breadcrumb">
+    <NuxtLink href="#" v-for="(link, i) in links" :key="i" :to="link.to">
+      {{ link.text }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { links: { default: [], type: Array } },
+};
+</script>
