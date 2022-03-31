9f4497b98d43e848f155e6332af6a83f056a4b12
berkinakkaya <berkin_akkaya@hotmail.com>
2022-03-31 21:42:41 +0300

    blog placeholders

--- pages/blog.vue
+++ pages/blog.vue
@@ -3,6 +3,14 @@ section.top {
   position: relative;
   overflow: visible !important;
 
+  @media (min-width: 1600px) {
+    --section-padding-inline: 16vw;
+  }
+
+  @media (max-width: 700px) {
+    text-align: center;
+  }
+
   h1 {
     margin-top: 50px;
 
@@ -42,11 +50,157 @@ section.top {
 
   img.abstract {
     position: absolute;
-    right: 0;
+    right: var(--section-padding-inline);
     top: 0;
     width: 402px;
     z-index: -1;
-    transform: translate(-50%, -50px);
+    transform: translateY(-50px);
+    max-width: 80%;
+
+    @media (max-width: 1150px) {
+      opacity: 0.5;
+    }
+
+    @media (max-width: 900px) {
+      left: 50%;
+      transform: translate(-50%, -50px);
+      opacity: 0.2;
+      display: none;
+    }
+  }
+}
+
+section.content {
+  margin-top: 150px;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    margin-bottom: 50px;
+
+    .category {
+      color: #424242;
+      font-weight: 700;
+      position: relative;
+      text-align: center;
+
+      &.active {
+        color: #ff5777;
+        font-size: 1.1em;
+
+        &::after {
+          content: '';
+          width: calc(100% + 20px);
+          display: block;
+          background: red;
+          height: 1px;
+          margin-top: 4px;
+          position: relative;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+      }
+    }
+  }
+
+  main {
+    display: grid;
+    grid-template-areas:
+      'a b'
+      'a c';
+    height: 400px;
+    gap: 20px;
+
+    @media (max-width: 900px) {
+      grid-template-columns: 1fr;
+      grid-template-areas: unset;
+      height: unset;
+    }
+
+    .article {
+      width: 100%;
+      height: 100%;
+
+      @media (max-width: 900px) {
+        grid-area: unset !important;
+        height: 200px;
+      }
+
+      &:nth-child(1) {
+        grid-area: a;
+      }
+      &:nth-child(2) {
+        grid-area: b;
+      }
+      &:nth-child(3) {
+        grid-area: c;
+      }
+    }
+  }
+
+  .article {
+    background: white;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 12px;
+  }
+
+  .article-group {
+    margin-top: 80px;
+
+    .head {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      @media (max-width: 600px) {
+        flex-direction: column;
+      }
+
+      .heading {
+        font-weight: 700;
+        font-size: 1.4em;
+        position: relative;
+
+        &::after {
+          content: '';
+          display: inline-block;
+          width: 35px;
+          height: 1px;
+          position: absolute;
+          top: 50%;
+          transform: translate(20px, -50%);
+          background: black;
+
+          @media (max-width: 600px) {
+            display: none;
+          }
+        }
+      }
+
+      a {
+        font-weight: 500;
+        color: black;
+        font-size: 1.2em;
+      }
+    }
+
+    .articles {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: 20px;
+      margin-top: 20px;
+      align-items: center;
+
+      @media (max-width: 900px) {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      @media (max-width: 600px) {
+        grid-template-columns: 1fr;
+      }
+    }
   }
 }
 </style>
@@ -84,6 +238,39 @@ section.top {
       <img src="../static/blog-abstract.svg" class="abstract" />
     </section>
 
+    <section class="content">
+      <nav>
+        <span class="category" :class="{ active: activeCategory == '' }" @click="activeCategory = ''">All</span>
+        <span
+          class="category"
+          :class="{ active: activeCategory == category }"
+          v-for="category in categories"
+          :key="category"
+          @click="activeCategory = category"
+        >
+          {{ category }}
+        </span>
+      </nav>
+
+      <main>
+        <div class="article"></div>
+        <div class="article"></div>
+        <div class="article"></div>
+      </main>
+
+      <div class="article-group featured" v-for="_ in 3" :key="_">
+        <div class="head">
+          <h4 class="heading">Featured Article</h4>
+
+          <a href="#">See All Articles &nbsp; <fa :icon="['fas', 'angle-right']" /></a>
+        </div>
+
+        <div class="articles">
+          <div class="article" style="height: 300px" v-for="_ in 4" :key="_"></div>
+        </div>
+      </div>
+    </section>
+
     <Footer />
   </div>
 </template>
@@ -94,5 +281,9 @@ import Footer from '~/components/Footer.vue';
 import Breadcrumb from '~/components/Pieces/Breadcrumb.vue';
 export default {
   components: { Header, Footer, Breadcrumb },
+  data: () => ({
+    activeCategory: '',
+    categories: ['UI Design', 'UX Design', 'Product Design', 'Articles', 'Tutorials', 'News'],
+  }),
 };
 </script>
